12da8166f0d795ebc10dc79b79133da2017f7f17
Bugra <seyitbugraerden@gmail.com>
2024-02-09 14:11:52 +0300

    prime icons library

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 
 function InputArea() {
   return (
     <>
       <h4>Input Area</h4>
       <InputText />
+      <Button icon="pi pi-check" />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { PrimeReactProvider } from "primereact/api";
 import "./index.css";
+import "primeicons/primeicons.css";
 import "primereact/resources/themes/viva-light/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
