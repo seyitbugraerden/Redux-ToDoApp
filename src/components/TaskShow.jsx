e214a48fcc0e189ca0c1dc96a83f235a1b8dae87
Bugra <seyitbugraerden@gmail.com>
2024-02-09 17:46:48 +0300

    delete animation

--- src/components/TaskShow.jsx
+++ src/components/TaskShow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Panel } from "primereact/panel";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTask } from "./Slice/TaskSlice";
@@ -6,17 +6,24 @@ import { deleteTask } from "./Slice/TaskSlice";
 function TaskShow() {
   const { data } = useSelector((store) => store.task);
   const dispatch = useDispatch();
+
+  const handleTaskDelete = (taskId) => {
+    const taskElement = document.getElementById(taskId);
+    if (taskElement) {
+      taskElement.classList.add("fade-out");
+      setTimeout(() => {
+        dispatch(deleteTask(taskId));
+      }, 500);
+    }
+  };
+
   return (
     <>
       <Panel header="Tasks">
         {data.map((item, index) => (
-          <div key={index} className="task">
+          <div key={index} className="task" id={item.id}>
             <p>{item.name}</p>
-            <button
-              onClick={() => {
-                dispatch(deleteTask(item.id));
-              }}
-            >
+            <button onClick={() => handleTaskDelete(item.id)}>
               Tamamlandı
             </button>
           </div>
